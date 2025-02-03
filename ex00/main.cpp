693583ed624cd8891397d60b2d4c652ab36689ca
Dilara Guler <dilaraguler@MacBook-Air-de-Dilara.local>
2025-02-03 18:52:12 +0100

    ex00 ok

---
+++ ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "easyfind.hpp"
+
+int main()
+{
+    std::vector<int> v;
+    for (int i = 0; i < 10; i++)
+        v.push_back(i);
+
+    try {
+        std::cout << "Found: " << *easyfind(v, 5) << std::endl;
+        std::cout << "Found: " << *easyfind(v, 10) << std::endl;
+    } 
+    catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
